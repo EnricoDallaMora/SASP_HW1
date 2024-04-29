fb3fa06c1231155af4d788838322e682fad76d95
Sness00 <gabriele.baroli@mail.polimi.it>
2024-04-29 18:01:27 +0200

    aggiunto un plot e reso più sobrio l'array di mics

--- main.m
+++ main.m
@@ -53,15 +53,28 @@ abs_p_avg = abs(p_avg);
 abs_p_avg = abs_p_avg ./ max(abs_p_avg, [], 2);
 [tax, theax] = meshgrid(theta, (0:1:N_frames-1)*big_hop/fs);
 figure
+subplot(2, 1, 1)
 surf(theax, tax, abs_p_avg, 'EdgeAlpha', 0)
 view(2)
 yticks([-90 -60 -30 0 30 60 90])
+ylim([-90 90])
 xticks(0:2:14)
+xlim([0 14.6])
 xlabel('time [s]')
-ylabel('DOA [deg]')
-xlim([-0.5 15])
+ylabel('\theta [deg]')
 title('Normalized pseudo-spectrum over time')
 colorbar
+
+subplot(2, 1, 2)
+plot((0:1:N_frames-1)*big_hop/fs, avg_theta, 'LineWidth', 1.2)
+xticks(0:2:14)
+xlim([0 14.6])
+yticks([-90 -60 -30 0 30 60 90])
+ylim([-90 90])
+xlabel('time [s]')
+ylabel('\theta_{dir} [deg]')
+title('Estimated DOA')
+grid on
 
 %% STATIC PLOT
 [u, v] = pol2cart(avg_theta*pi/180+pi/2, 0.25);
@@ -72,17 +85,18 @@ circle = sqrt(-x_c.^2+(0.25)^2);
 degrees = {'-90°', '-60°', '-30°', '0°', '30°', '60°', '90°'};
 
 figure
-plot(x_c, circle, 'Color', 'k', 'LineWidth', 1.2, 'LineStyle', '--')
+plot(x_c, circle, 'Color', 'k', 'LineWidth', 1.1, 'LineStyle', '--')
 for ll = 0:6
-    line([0 0.27*cos(pi/6*ll)], [0 0.27*sin(pi/6*ll)], 'Color', 'k', 'LineWidth', 1.2, 'LineStyle', '--')
+    line([0 0.27*cos(pi/6*ll)], [0 0.27*sin(pi/6*ll)], 'Color', 'k', 'LineWidth', 1.1, 'LineStyle', '--')
     text(0.29*cos(pi/6*ll), 0.29*sin(pi/6*ll), degrees(ll+1), 'HorizontalAlignment', 'center')
 end
 hold on
-quiver(zeros(1, length(avg_theta)), zeros(1, length(avg_theta)), u, v, 'LineStyle','-', ...
-    'Color', 'red', 'LineWidth', 1.2)
+plot(linspace(-0.45/2, 0.45/2, 16), zeros(1, 16),  'LineStyle', 'none', 'Marker', 'o', 'Color', 'b', 'MarkerFaceColor', 'b', ...
+    'MarkerSize', 8);
 hold on
-plot(linspace(-0.45/2, 0.45/2, 16), zeros(1, 16),  'LineStyle', 'none', 'Marker','diamond', ...
-    'MarkerFaceColor','red', 'MarkerSize', 10, 'Color','#666666', 'LineWidth', 2);
+quiver(zeros(1, length(avg_theta)), zeros(1, length(avg_theta)), u, v, 'LineStyle','-', ...
+    'Color', 'r', 'LineWidth', 1.2)
+
 xlim([-0.3 0.3])
 ylim([-0.1 0.4])
 yticks([])
@@ -104,16 +118,15 @@ for i = 1:length(avg_theta)
         text(0.29*cos(pi/6*ll), 0.29*sin(pi/6*ll), degrees(ll+1), 'HorizontalAlignment', 'center')
     end
     hold on
-    plot(linspace(-0.45/2, 0.45/2, 16), zeros(1, 16),  'LineStyle', 'none', ...
-        'Marker','diamond', 'MarkerFaceColor','red', 'MarkerSize', 10, ...
-        'Color','#666666', 'LineWidth',2);
+    plot(linspace(-0.45/2, 0.45/2, 16), zeros(1, 16),  'LineStyle', 'none', 'Marker', 'o', 'Color', 'b', ...
+        'MarkerFaceColor', 'b', 'MarkerSize', 8)
     hold on
     quiver(0, 0, u(i), v(i), 'LineStyle','-', 'Color', 'red', 'LineWidth', 2, 'MaxHeadSize', 0.3)
     hold off
     xlim([-0.25 0.25])
     ylim([-0.1 0.4])
     axis equal
-    title(['Estimated DOA at time ', num2str(round(big_hop*i/fs, 3), 3), ' [s]'])
+    title(['Estimated DOA at time ', num2str(big_hop*i/fs, '%.2f'), ' [s]'])
     xlabel('array elements coordinates [m]')
     xticks([-22.5e-2 -11.25e-2 0 11.25e-2 22.5e-2])
     yticks([])
